a8be79d6d60bbd172a8d5e3288fa7b8b22cbef07
Shashank Sharma <shashankrnr32@gmail.com>
2018-11-03 21:43:30 +0530

    03112018 2143 mfcc,delta,filterbank

--- Python Scripts/Main.py
+++ Python Scripts/Main.py
@@ -2,7 +2,6 @@
 import PyPlot as Plot
 import PyWavRead as WavRead
 import PySelectF as FileSelect
-import matplotlib.pyplot as plt
 #==============================================================================
 from python_speech_features import mfcc
 from python_speech_features import delta
@@ -13,7 +12,7 @@ from python_speech_features import logfbank
 
 file_path = FileSelect.selectFile()
 fs,signal = WavRead.readWAVFile(file_path.name)
-mfcc_feat = mfcc(signal,fs,nfft=1103)
+mfcc_feat = mfcc(signal,fs,nfft=2048)
 
 Plot.plotSpectrogram(signal,fs)
 Plot.plotY(signal/max(signal))
@@ -21,12 +20,10 @@ Plot.plotY(signal/max(signal))
 d_mfcc_feat = delta(mfcc_feat, 2)
 d_mfcc_feat_2=delta(d_mfcc_feat,2)
 
-plt.plot(mfcc_feat)
-plt.show()
-plt.plot(d_mfcc_feat)
-plt.show()
-plt.plot(d_mfcc_feat_2)
-plt.show()
-fbank_feat = logfbank(signal,fs)
+Plot.plotY(mfcc_feat)
+Plot.plotY(d_mfcc_feat)
+Plot.plotY(d_mfcc_feat_2)
+
+fbank_feat = logfbank(signal,fs,nfft=2048)
 
 print(fbank_feat[1:3,:])
